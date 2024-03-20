f3f2984fba648609418d3895f01dd9933a4403b4
Martin Kondor <martinkondor@gmail.com>
2024-03-20 11:48:09 +0100

    tailor react

--- web/views.py
+++ web/views.py
@@ -15,48 +15,57 @@ def login(request):
     Log in the user if the password is correct
     """
     if request.method == "POST":
-        pass
-    return Response({'message': 'login'})
+        return Response({'status': '0'})
+    return Response({'status': '1'})
 
 
+@api_view(['GET', 'POST'])
 def signup(request):
     """
     Register the user if the given email is not
     already in use
     """
     if request.method == "POST":
-        pass
-    return HttpResponse("signup")
+        return Response({'status': '0'})
+    return Response({'status': '1'})
 
 
+@api_view(['POST'])
 def logout(request):
     """
     Log out the user
     """
-    return HttpResponse("logout")
+    if request.method == "POST":
+        return Response({'status': '0'})
+    return Response({'status': '1'})
 
 
+@api_view(['GET', 'POST'])
 def settings(request):
     """
     The user is able to set it's email, and can delete itself
     """
     if request.method == "POST":
-        pass
-    return HttpResponse("settings")
+        return Response({'status': '0'})
+    return Response({'status': '1'})
 
 
+@api_view(['GET', 'POST'])
 def index(request):
     """
     The user can search for other users in the database
     by their emails
     """
-    return HttpResponse("index")
+    if request.method == "POST":
+        return Response({'status': '0'})
+    return Response({'status': '1'})
 
 
+@api_view(['GET', 'POST'])
 def chat(request, id_or_email):
     """
     Chat with the other person based on its id_or_email
     """
     if request.method == "POST":
-        pass
-    return HttpResponse("chat_by_email")
+        return Response({'status': '0'})
+    return Response({'status': '1'})
